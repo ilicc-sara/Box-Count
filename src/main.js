adcdfb4adec042b764d9748b1668209a5656fe94
Sara <sarailic431@gmail.com>
2025-01-20 19:41:26 +0100

    finished

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const totalNum = document.querySelector(".total-boxes-num");
 function boxCreator() {
   let id = crypto.randomUUID();
   let counter = 0;
-  // variabla let id
+
   const getId = () => id;
   const getCounter = () => counter;
 
@@ -21,11 +21,10 @@ function boxCreator() {
   return { getId, getCounter, increaseCounter, decreaseCounter };
 }
 
-// const box = boxCreator();
-
 function boxManagerCreator() {
   const boxes = [];
   let boxesCounter = 0;
+  let totalCounter = 0;
 
   const addToBoxes = (value) => boxes.push(value);
   const getBoxes = () => boxes;
@@ -33,7 +32,11 @@ function boxManagerCreator() {
   const increaseboxesCounter = () => (boxesCounter += 1);
   const getboxesCounter = () => boxesCounter;
 
-  return { addToBoxes, getBoxes, increaseboxesCounter, getboxesCounter };
+  const getTotalCounter = () => totalCounter;
+  const increaseTotalCounter = () => (totalCounter += 1);
+  const decreaseTotalCounter = () => (totalCounter -= 1);
+  // prettier-ignore
+  return { addToBoxes, getBoxes, increaseboxesCounter, getboxesCounter, getTotalCounter, increaseTotalCounter, decreaseTotalCounter };
 }
 
 const boxManager = boxManagerCreator();
@@ -41,21 +44,21 @@ const boxManager = boxManagerCreator();
 addBtn.addEventListener("click", function (e) {
   const box = boxCreator();
 
-  // if (boxManager.getBoxes().length < 10) {
-  boxManager.addToBoxes(box);
-  const item = document.createElement("div");
-  item.classList.add("box");
-  item.setAttribute("data-id", box.getId());
-  item.innerHTML = `
+  if (boxManager.getBoxes().length < 10) {
+    boxManager.addToBoxes(box);
+    const item = document.createElement("div");
+    item.classList.add("box");
+    item.setAttribute("data-id", box.getId());
+    item.innerHTML = `
     <button class="btn subtract">-</button>
-    <button id="${box.getId()}" class="btn num">0</button>
+    <button id="${box.getId()}" class="btn num">${box.getCounter()}</button>
     <button class="btn add">+</button>
     <ion-icon class="icon" name="trash-bin-outline"></ion-icon>
     `;
-  boxCont.appendChild(item);
+    boxCont.appendChild(item);
 
-  boxManager.increaseboxesCounter();
-  // }
+    boxManager.increaseboxesCounter();
+  }
 
   emptyText.classList.add("hidden");
   boxesNum.textContent = boxManager.getboxesCounter();
@@ -75,10 +78,13 @@ boxCont.addEventListener("click", function (e) {
 
   if (e.target.classList.contains("subtract")) {
     targetBox.decreaseCounter();
+    boxManager.decreaseTotalCounter();
   }
 
   if (e.target.classList.contains("add")) {
     targetBox.increaseCounter();
+    boxManager.increaseTotalCounter();
   }
   element.textContent = targetBox.getCounter();
+  totalNum.textContent = boxManager.getTotalCounter();
 });
